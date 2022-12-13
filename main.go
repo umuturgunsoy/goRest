acb148a9f373714753bb2232834f9abb377dc321
umutus <u.urgunsoy@digitastic.app>
2022-12-13 20:03:55 +0300

    Allah cezanı versin gpt

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Selamlar")
-	})
-	http.ListenAndServe(":8080", nil)
+	f1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "birinci func \n ")
+	}
+
+	f2 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "ikinci func \n ")
+	}
+
+	http.HandleFunc("/", f1)
+	http.HandleFunc("/f2", f2)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
